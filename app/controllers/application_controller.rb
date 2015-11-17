a77aef39f990f6755d21a864e674dcb759fb87da
Jimmy Zeng <jzengg@gmail.com>
2015-11-17 09:55:01 -0500

    User auth and static page with root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?, :require_login
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def sign_in!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out!
+    current_user.reset_session_token!
+    session[:sesison_token] = nil
+  end
+
+  def require_login
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+  validates :username, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user.try(:is_password?, password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    self.password_digest.is_password?(password)
+  end
+
+  def password_digest
+    BCrypt::Password.new(super)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
 end
