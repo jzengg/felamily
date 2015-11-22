521f5345668a16871c7c4c82bfa9432da8c37303
Jimmy Zeng <jzengg@gmail.com>
2015-11-21 20:00:54 -0500

    Validate username unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120225452) do
+ActiveRecord::Schema.define(version: 20151122003226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,5 +37,7 @@ ActiveRecord::Schema.define(version: 20151120225452) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
