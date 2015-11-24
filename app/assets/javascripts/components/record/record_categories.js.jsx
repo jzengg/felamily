e7ce32387d934108513591209768eba2fc4f6468
Jimmy Zeng <jzengg@gmail.com>
2015-11-24 09:40:16 -0500

    Styling fixes

--- app/assets/javascripts/components/record/record_categories.js.jsx
+++ app/assets/javascripts/components/record/record_categories.js.jsx
@@ -3,13 +3,25 @@ var Link = ReactRouter.Link;
 var RecordCategories = React.createClass({
   mixins: [ReactRouter.History],
 
+  getInitialState: function () {
+    return {active: 0};
+  },
+
+  handleClick: function (i) {
+    this.setState({active: i});
+  },
+
   render: function () {
     var cat = this.props.cat;
     var extra = ["medication", "test"];
     var categories = ["animal", "vaccine"];
     var links = categories.map(function (category, i) {
-      return <li key={i}>  <Link to={"/cats/" + cat.id + "/" + category}> {category} </Link> </li>;
-    });
+      var style = "";
+      if (this.state.active == i) {
+        style = "active-category";
+      }
+        return <li key={i}>  <Link className={style} onClick={this.handleClick.bind(null, i)} to={"/cats/" + cat.id + "/" + category}> {category} </Link> </li>;
+    }.bind(this));
     return(
       <ul className="record-categories group">
         {links}
